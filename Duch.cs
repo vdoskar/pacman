0a1cae8d35b56e09e1508c8964a12c2a6e748a8a
Krisp1k <vladadoskar@gmail.com>
2024-04-28 00:11:50 +0200

    migration from old repo

---
+++ Duch.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace pacman
+{
+    internal class Duch
+    {
+        public Ellipse textura;
+        protected int[,] waypointy;
+
+        int velX = 0;
+        int velY = 0;
+        public double moveDelay = 5;
+        private double sDelay = 0;
+
+
+
+        private void prolez(int souradniceX, int souradniceY, int pocetKroku, bool[,] mapa)
+        {
+
+            if (mapa[souradniceX, souradniceY]) 
+            { 
+                if (waypointy[souradniceX, souradniceY] > pocetKroku) 
+                {
+                    waypointy[souradniceX, souradniceY] = pocetKroku;
+
+                    prolez(souradniceX, souradniceY + 1, pocetKroku + 1, mapa);
+                    prolez(souradniceX, souradniceY - 1, pocetKroku + 1, mapa);
+                    prolez(souradniceX + 1, souradniceY, pocetKroku + 1, mapa);
+                    prolez(souradniceX - 1, souradniceY, pocetKroku + 1, mapa);
+                }
+            }
+        }
+
+        public virtual void nastavWaypointy(bool[,] mapa, Point souradnicePacmana)
+        {
+            waypointy = new int[mapa.GetLength(0), mapa.GetLength(1)];
+
+            for (int i = 0; i < waypointy.GetLength(0); i++)
+            {
+                for (int j = 0; j < waypointy.GetLength(1); j++)
+                {
+                    waypointy[i, j] = int.MaxValue;
+                }
+            }
+
+            prolez((int)souradnicePacmana.X, (int)souradnicePacmana.Y, 0, mapa);
+        }
+
+        public virtual void pohyb() //bool[,] mapa, Point souradnicePacmana
+        {
+            if (this.waypointy != null)
+            {
+                sDelay++;
+
+                if (sDelay == moveDelay)
+                { 
+                    sDelay = 0;
+
+                } else {
+                    Thickness pozice = this.textura.Margin;
+
+                    if ((pozice.Left % 20 == 0) && (pozice.Top % 20 == 0))
+                    {
+                        int pSloupec = Convert.ToInt32(pozice.Left / 20);
+                        int pRadek = Convert.ToInt32(pozice.Top / 20);
+
+                        //MessageBox.Show(pSloupec + " " + pRadek);
+
+                        int[] smery = new int[4];
+                        smery[0] = waypointy[pSloupec , pRadek - 1];
+                        smery[1] = waypointy[pSloupec + 1, pRadek];
+                        smery[2] = waypointy[pSloupec , pRadek + 1];
+                        smery[3] = waypointy[pSloupec - 1, pRadek ];
+
+                        int minimum = 0;
+
+                        for (int i = 0; i < smery.Length; i++)
+                            if (smery[i] < smery[minimum])
+                                minimum = i;
+
+                        switch(minimum)
+                        {
+                            case 0: this.velX = 0; this.velY = -1; break; //nahoru
+                            case 1: this.velX = 1; this.velY = 0; break; //doprava
+                            case 2: this.velX = 0; this.velY = 1; break; //dolů
+                            case 3: this.velX = -1; this.velY = 0; break; //doleva
+                        }
+                    }
+
+                    pozice.Left += this.velX;
+                    pozice.Top += this.velY;
+
+                    this.textura.Margin = pozice;
+                
+                }
+            }
+        }
+        
+        public Duch(int col, int row)
+        {
+            this.textura = new Ellipse();
+            this.textura.Width = 20;
+            this.textura.Height = 20;
+            this.textura.Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            this.textura.VerticalAlignment = VerticalAlignment.Top;
+            this.textura.HorizontalAlignment = HorizontalAlignment.Left;
+            this.textura.Margin = new Thickness(col * 20, row * 20, 0, 0);
+            Canvas.SetZIndex(this.textura, 11);
+        }
+    }
+
+    class JinyDuch:Duch
+    {
+        public JinyDuch(int sloupec, int radek):base(sloupec, radek)
+        {
+
+        }
+
+        public override void nastavWaypointy(bool[,] mapa, Point souradnicePacmana)
+        {
+            waypointy = new int[mapa.GetLength(0), mapa.GetLength(1)];
+
+            for (int i = 0; i < waypointy.GetLength(0); i++)
+            {
+                for (int j = 0; j < waypointy.GetLength(1); j++)
+                {
+                    waypointy[i, j] = int.MaxValue;
+                }
+            }
+
+            prolez((int)souradnicePacmana.X, (int)souradnicePacmana.Y, 0, mapa);
+        }
+
+        private void prolez(int souradniceX, int souradniceY, int pocetKroku, bool[,] mapa)
+        {
+
+            if (mapa[souradniceX, souradniceY])
+            {
+                if (waypointy[souradniceX, souradniceY] > pocetKroku)
+                {
+                    waypointy[souradniceX, souradniceY] = pocetKroku;
+                    prolez(souradniceX, souradniceY - 1, pocetKroku + 1, mapa);
+                    prolez(souradniceX, souradniceY + 1, pocetKroku + 1, mapa);
+                    prolez(souradniceX - 1, souradniceY, pocetKroku + 1, mapa);
+                    prolez(souradniceX + 1, souradniceY, pocetKroku + 1, mapa);
+                }
+            }
+        }
+    }
+    class DementDuch : Duch
+    {
+        int inteligence = 0;
+        Random rand = new Random();
+
+
+        public DementDuch(int sloupec, int radek, int inteligence) : base(sloupec, radek)
+        {
+            this.inteligence = inteligence;
+        }
+
+        public override void pohyb()
+        {
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace pacman
+{
+    public partial class MainWindow : Window
+    {
+
+        DispatcherTimer casovac = new DispatcherTimer();
+        Random rand = new Random();
+
+
+        double RYCHLOST_PACMAN = 2.5;
+
+        bool nahoru = false;
+        bool dolu = false;
+        bool doleva = false;
+        bool doprava = false;
+
+        int pixelNaPolicko = 20;
+        int pocetPuntiku;
+        int pocetSezranychPuntiku = 0;
+
+        int smer = 2;
+        int uzivatelSmer = 1;
+
+        static int rows = 0;
+        static int cols = 0;
+
+        bool[,] plocha_log = new bool[rows, cols];
+        Ellipse[,] puntiky = new Ellipse[rows, cols];
+
+        List<Duch> Duchove = new List<Duch>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // VYTVOŘENÍ POLE
+
+            StreamReader sr = new StreamReader("mapa.txt");
+
+            cols = Convert.ToInt32(sr.ReadLine());
+            rows = Convert.ToInt32(sr.ReadLine());
+            
+            plocha_log = new bool[rows, cols];
+            puntiky = new Ellipse[rows, cols];
+
+            for (int j = 0; j < cols; j++)
+                for (int i = 0; i < rows; i++)
+                {
+                    switch (Convert.ToInt32(sr.ReadLine()))
+                    {
+                        case 1: plocha_log[i, j] = true;
+
+                            pocetPuntiku++;
+                            puntiky[i, j] = new Ellipse();
+                            puntiky[i, j].Width = 3;
+                            puntiky[i, j].Height = 3;
+                            puntiky[i, j].Fill = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                            puntiky[i, j].VerticalAlignment = VerticalAlignment.Top;
+                            puntiky[i, j].HorizontalAlignment = HorizontalAlignment.Left;
+                            puntiky[i, j].Margin = new Thickness(pixelNaPolicko * i + 9, pixelNaPolicko * j + 9, 0, 0);
+
+                            Canvas.SetZIndex(puntiky[i, j], 10);
+                            plocha.Children.Add(puntiky[i, j]);
+
+                            break;
+
+                        case 2: plocha_log[i, j] = true;
+
+                            pacman.Margin = new Thickness(pixelNaPolicko * i, pixelNaPolicko * j, 0, 0);
+                            Canvas.SetZIndex(pacman, 10);
+                            puntiky[i, j] = new Ellipse();
+                            puntiky[i, j].Visibility = Visibility.Hidden;
+
+                            break;
+
+                        case 3: plocha_log[i, j] = true;
+
+                            Duch novyDuch = new Duch(i, j);
+                            novyDuch.moveDelay = rand.Next(2, 6);
+                            Duchove.Add(novyDuch);
+
+                            pocetPuntiku++;
+                            puntiky[i, j] = new Ellipse();
+                            puntiky[i, j].Width = 3;
+                            puntiky[i, j].Height = 3;
+                            puntiky[i, j].Fill = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                            puntiky[i, j].VerticalAlignment = VerticalAlignment.Top;
+                            puntiky[i, j].HorizontalAlignment = HorizontalAlignment.Left;
+                            puntiky[i, j].Margin = new Thickness(pixelNaPolicko * i + 9, pixelNaPolicko * j + 9, 0, 0);
+
+                            Canvas.SetZIndex(puntiky[i, j], 10);
+                            plocha.Children.Add(puntiky[i, j]);
+
+                            break;
+
+                        case 4:
+                            plocha_log[i, j] = true;
+
+                            JinyDuch JinyDuch = new JinyDuch(i, j);
+                            JinyDuch.moveDelay = rand.Next(2, 6);
+                            Duchove.Add(JinyDuch);
+
+                            pocetPuntiku++;
+                            puntiky[i, j] = new Ellipse();
+                            puntiky[i, j].Width = 3;
+                            puntiky[i, j].Height = 3;
+                            puntiky[i, j].Fill = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                            puntiky[i, j].VerticalAlignment = VerticalAlignment.Top;
+                            puntiky[i, j].HorizontalAlignment = HorizontalAlignment.Left;
+                            puntiky[i, j].Margin = new Thickness(pixelNaPolicko * i + 9, pixelNaPolicko * j + 9, 0, 0);
+
+                            Canvas.SetZIndex(puntiky[i, j], 10);
+                            plocha.Children.Add(puntiky[i, j]);
+
+                            break;
+
+                        default: 
+
+                            plocha_log[i, j] = false; 
+                            break;
+                    } 
+                }
+
+            currentPointsTextBlock.Text = "Zbývá sežrat : " + pocetPuntiku;
+
+            for (int i = 0; i < plocha_log.GetLength(0); i++)
+                for (int j = 0; j < plocha_log.GetLength(1); j++)
+                {
+                    Rectangle policko = new Rectangle();
+                    policko.Width = pixelNaPolicko;
+                    policko.Height = pixelNaPolicko;
+                    policko.VerticalAlignment = VerticalAlignment.Top;
+                    policko.HorizontalAlignment = HorizontalAlignment.Left;
+                    policko.Margin = new Thickness(i * 20, j * 20, 0, 0);
+
+                    if (plocha_log[i, j] == true) {
+                        policko.Fill = new SolidColorBrush(Color.FromArgb(255,0,0,255));
+                    } else {
+                        policko.Fill = new SolidColorBrush(Color.FromArgb(255,0,0,0));
+                    }
+
+                    plocha.Children.Add(policko);
+                }
+
+            foreach (Duch jeden in Duchove)
+            {
+                plocha.Children.Add(jeden.textura);
+            }
+
+            // ČASOVAČ
+            casovac.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            casovac.Tick += akce;
+            casovac.Start();
+        }
+
+        bool kolize(Duch duch)
+        {
+            double vzdalenost_x = duch.textura.Margin.Left - pacman.Margin.Left;
+            double vzdalenost_y = duch.textura.Margin.Top - pacman.Margin.Top;
+            double vzdalenost = Math.Sqrt(Math.Pow(vzdalenost_x, 2) + Math.Pow(vzdalenost_y, 2)); // pythagorovka
+
+            if (vzdalenost < (pacman.Width / 2 + duch.textura.Width / 2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void akce(object sender, EventArgs e)
+        {
+
+            // pohyb pacmana
+
+            foreach(Duch duch in Duchove)
+            {
+                duch.pohyb();
+            }
+
+            Thickness pozicePackmana = pacman.Margin;
+
+            if ((pozicePackmana.Left % pixelNaPolicko == 0) && (pozicePackmana.Top % pixelNaPolicko == 0)) 
+            {
+                int pacmanSloupec = Convert.ToInt32(pozicePackmana.Left / pixelNaPolicko);
+                int pacmanRadek = Convert.ToInt32(pozicePackmana.Top / pixelNaPolicko);
+
+                foreach (Duch duch in Duchove)
+                {
+                    duch.nastavWaypointy(plocha_log, new Point(pacmanSloupec, pacmanRadek));
+                }
+
+                if (puntiky[pacmanSloupec, pacmanRadek].Visibility == Visibility.Visible)
+                {
+                    puntiky[pacmanSloupec, pacmanRadek].Visibility = Visibility.Hidden;
+                    pocetSezranychPuntiku++;
+                    currentPointsTextBlock.Text = "Zbývá sežrat : " + (pocetPuntiku - pocetSezranychPuntiku);
+                }
+
+                if (pocetSezranychPuntiku == pocetPuntiku)
+                {
+                    casovac.Stop();
+                    MessageBox.Show("win win");
+                }
+
+                // tady se nastavuje smer uzivatele
+                switch (uzivatelSmer)
+                {
+                    case 1  : if (plocha_log[pacmanSloupec, pacmanRadek - 1]) smer = 1;  break;
+                    case 2  : if (plocha_log[pacmanSloupec + 1, pacmanRadek]) smer = 2;  break; 
+                    case 3  : if (plocha_log[pacmanSloupec, pacmanRadek + 1]) smer = 3;  break;
+                    default : if (plocha_log[pacmanSloupec - 1, pacmanRadek]) smer = 4; break;
+                }
+
+                // 1 = nahoru, 2 = doprava, 3 = dolů, 4 = doleva
+                switch (smer)
+                {
+                    case 1 : if (plocha_log[pacmanSloupec, pacmanRadek - 1]) pozicePackmana.Top--; break;
+                    case 2 : if (plocha_log[pacmanSloupec + 1, pacmanRadek]) pozicePackmana.Left++; break;
+                    case 3 : if (plocha_log[pacmanSloupec, pacmanRadek + 1]) pozicePackmana.Top++; break;
+                    default: if (plocha_log[pacmanSloupec - 1, pacmanRadek]) pozicePackmana.Left--; break;
+                }
+
+                pacman.Margin = pozicePackmana; // ted to jede samo  
+            } 
+            else
+            {
+                switch (smer)  
+                {
+                    case 1 : pozicePackmana.Top--; break;
+                    case 2 : pozicePackmana.Left++; break;
+                    case 3 : pozicePackmana.Top++; break;
+                    default: pozicePackmana.Left--; break;
+                }
+                pacman.Margin = pozicePackmana;
+            } 
+
+            // test kolize
+
+            foreach (Duch duch in Duchove)
+            {
+                if (kolize(duch))
+                {
+                    casovac.Stop();
+                    MessageBox.Show("Game over lol");
+                }
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Up)    { uzivatelSmer = 1;   }
+            if (e.Key == Key.Down)  { uzivatelSmer = 3;   }
+            if (e.Key == Key.Right) { uzivatelSmer = 2;   }
+            if (e.Key == Key.Left)  { uzivatelSmer = 4;   }
+        }
+        
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Up)    { nahoru = false;   }
+            if (e.Key == Key.Down)  { dolu = false;     }
+            if (e.Key == Key.Right) { doprava = false;  }
+            if (e.Key == Key.Left)  { doleva = false;   }
+        }
+        
+    }
+}
